014e529f4696a9ba46931d1ee7ce10953cd107f9
Puji <2430438586@qq.com>
2025-05-18 23:59:52 +0800

    init

---
+++ src/main/java/puji/p2p_notes_sync/P2pNotesSyncApplication.java
@@ -0,0 +1,13 @@
+package puji.p2p_notes_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pNotesSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pNotesSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/puji/p2p_notes_sync/P2pNotesSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package puji.p2p_notes_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pNotesSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
